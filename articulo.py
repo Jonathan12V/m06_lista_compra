8365a31b512a981e4f331461d9dd1beefeb29f3a
Daniel C <Danielcontaf1996@gmail.com>
2023-10-31 17:30:52 +0100

    Articulo.py hecho en teoria

--- articulo.py
+++ articulo.py
@@ -1,3 +1,9 @@
+class articulo:
+    def __init__(self, nombre):
+        self._nombre = nombre
 
-class articulo():
-    pass
+    def get_nom(self):
+        return self._nombre
+
+    def set_nom(self, nuevo_nombre):
+        self._nombre = nuevo_nombre
