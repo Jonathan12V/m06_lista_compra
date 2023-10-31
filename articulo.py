f5a613c19acd4c5661e9fb24608964de2bc8335b
Daniel C <Danielcontaf1996@gmail.com>
2023-10-31 18:32:44 +0100

    Cambiar articulo a article

--- articulo.py
+++
@@ -1,9 +0,0 @@
-class articulo:
-    def __init__(self, nombre):
-        self._nombre = nombre
-
-    def get_nom(self):
-        return self._nombre
-
-    def set_nom(self, nuevo_nombre):
-        self._nombre = nuevo_nombre
